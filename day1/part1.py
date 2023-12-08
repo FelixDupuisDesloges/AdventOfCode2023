1d22f48588e48b7f92c4384cebf82969e8d14d0a
FelixDupuisDesloges <71955623+FelixDupuisDesloges@users.noreply.github.com>
2023-12-07 21:32:02 -0500

    part1v1

--- day1/part1.py
+++ day1/part1.py
@@ -1,6 +1,14 @@
 with open('inoutD1.txt', 'r') as myFile:
     lignes = myFile.readlines()
 
-ligne1 = lignes[0]
+somme = 0
 
-print(ligne1)
+for ligne in lignes:
+    for carac in ligne:
+        if carac.isnumeric():
+            somme += int(carac)*10
+    for carac in reversed(ligne):
+        if carac.isnumeric():
+            somme += int(carac)
+
+print(somme)
