70e7863a4af5d3b485618df1757b82ec94eab310
FelixDupuisDesloges <71955623+FelixDupuisDesloges@users.noreply.github.com>
2023-12-07 22:30:14 -0500

    t

--- day1/part1.py
+++ day1/part1.py
@@ -1,22 +1,39 @@
+import re
 with open('inoutD1.txt', 'r') as myFile:
     lignes = myFile.readlines()
 
 somme = 0
 
 for ligne in lignes:
-
+    num1 = None
+    num2 = None
+    indexN1 = None
+    indexN2 = None
+    
 
     for carac in ligne:
         if carac.isnumeric():
-            value = int(carac)*10
-            print(value)
-            somme += value
+            num1 = int(carac)
+            indexN1 = ligne.find(carac)
             break
+
     for carac in reversed(ligne):
         if carac.isnumeric():
-            value = int(carac)
-            print(value)
-            somme += value
+            num2 = int(carac)
+            indexN2 = reversed(ligne).find(carac)
             break
+
+    numInStr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    num =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    for i in range(0, len(numInStr)):
+        results = re.findall(numInStr[i], ligne)
+        if len(results) != 0:
+            if results[0] < indexN1:
+                num1 = i+1
+            if results[-1] > indexN2:
+                num2 = i+1
+    
+    print(num1*10+num2)
 
 print(somme)
