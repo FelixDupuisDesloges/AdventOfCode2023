8c67e1bbced363c28574b437a03f95ef83bf365e
FelixDupuisDesloges <71955623+FelixDupuisDesloges@users.noreply.github.com>
2023-12-07 21:41:51 -0500

    part1vF

--- day1/part1.py
+++ day1/part1.py
@@ -16,12 +16,12 @@ for carac in lignes[0]:
         value = int(carac)*10
         print(value)
         somme += value
-        continue
+        break
 for carac in reversed(lignes[0]):
     if carac.isnumeric():
         value = int(carac)
         print(value)
         somme += value
-        continue
+        break
 
 print(somme)
